0d6b213514a78e168bc2727cee6dcded34fa7521
Victor Liang <victor.zw.liang@gmail.com>
2020-06-04 02:31:58 +0000

    Words search pair programming

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -35,4 +35,68 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+
+  it("should return true if the word is present vertically", function() {
+    const result = wordSearch([
+      ['A', 'S', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'O', 'P', 'F', 'E', 'L', 'D'],
+      ['Y', 'I', 'D', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'N', 'E', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'F', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'E', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'L', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'CODE')
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present horizontally but backwards", function() {
+    const result = wordSearch([
+      ['A', 'S', 'E', 'D', 'O', 'C', 'A', 'L'],
+      ['S', 'E', 'O', 'P', 'F', 'E', 'L', 'D'],
+      ['Y', 'I', 'D', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'N', 'F', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'F', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'E', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'L', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'CODE')
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present vertically but backwards", function() {
+    const result = wordSearch([
+      ['A', 'S', 'E', 'D', 'D', 'C', 'A', 'L'],
+      ['S', 'E', 'O', 'P', 'F', 'E', 'L', 'D'],
+      ['Y', 'I', 'D', 'F', 'Q', 'D', 'A', 'L'],
+      ['H', 'N', 'F', 'T', 'E', 'O', 'R', 'G'],
+      ['W', 'F', 'C', 'S', 'Y', 'C', 'R', 'L'],
+      ['B', 'E', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'L', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'CODE')
+
+    assert.isTrue(result);
+  });
+
+  // it("should return true if the word is present diagonally downwards to the right", function() {
+  //   const result = wordSearch([
+  //     ['A', 'S', 'E', 'D', 'D', 'C', 'A', 'L'],
+  //     ['S', 'E', 'O', 'P', 'F', 'E', 'L', 'D'],
+  //     ['Y', 'I', 'C', 'F', 'Q', 'D', 'G', 'L'],
+  //     ['H', 'N', 'F', 'O', 'E', 'N', 'R', 'G'],
+  //     ['W', 'F', 'C', 'S', 'D', 'O', 'R', 'L'],
+  //     ['B', 'E', 'R', 'F', 'N', 'I', 'Y', 'B'],
+  //     ['U', 'L', 'O', 'W', 'A', 'P', 'N', 'I'],
+  //     ['O', 'C', 'C', 'A', 'K', 'U', 'A', 'G'],
+  //     ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+  //   ], 'CODING')
+
+  //   assert.isTrue(result);
+  // });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,26 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+    const reversed = word.split('').reverse().join('');
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    const verticalJoin = [];
+    
+    for (let i = 0; i < letters[0].length; i++) {
+        let string = "";
+        letters.forEach(subArr => string += subArr[i]);
+        verticalJoin.push(string);
     }
+    
+    for (const l of horizontalJoin) {
+        if (l.includes(word) || l.includes(reversed)) return true;
+    }
+    for (const l of verticalJoin) {
+        if (l.includes(word) || l.includes(reversed)) {
+            return true;
+        }
+    }
+
+
+    return false; 
 }
+
 
 module.exports = wordSearch
